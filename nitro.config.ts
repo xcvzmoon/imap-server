1491e574bb43930aeb9ef8f3884fd71d1bc11b95
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-01-03 19:34:22 +0800

    chore: use bun runtime

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,8 +1,8 @@
 import { defineNitroConfig } from 'nitropack/config';
 
-// https://nitro.build/config
 export default defineNitroConfig({
   compatibilityDate: 'latest',
   srcDir: 'server',
   imports: false,
+  preset: 'bun',
 });
